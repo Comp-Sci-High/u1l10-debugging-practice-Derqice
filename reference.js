f9576f22aaa2fd0c939e0d5030505aa24a99b2a4
Derqice <139995816+Derqice@users.noreply.github.com>
2025-09-29 19:13:18 +0000

    Finished

--- reference.js
+++ reference.js
@@ -10,37 +10,37 @@
 
 
 
-// console.log(lesson)
-// const lesson = "U1L9";
+
+const lesson = "U1L9";
+console.log(lesson)
+
+
+
+const activity = "practice";
+console.log(activity)
 
 
 
 
-// const activity = "practice";
-// console.log(actrivity)
-
-
-
-
-// function greet(name) {
-//     console.log("Hello " + name);
-// }
-// console.log(freet("Cesar"))
+function greet(name) {
+    console.log("Hello " + name);
+}
+greet("Cesar")
 
 
 
 
 
-// function car() {
-//     let model = "Tesla";
-    
-//     if (model === "Tesla") {
-//         let year = 2021;
-//     } else {
-//         let year = 2024;
-//     }
+function car() {
+    let model = "Tesla";
+    let year;
+    if (model == "Tesla") {
+         year = 2021;
+    } else {
+         year = 2024;
+    }
 
-//     return year
-// }
+    return year
+}
 
-// car();
+car();
